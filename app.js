df50fc89fdfdd376ce5a7db868dfb2829c286ef6
820178112 <qq820178112@163.com>
2020-09-29 19:01:01 +0800

    小程序登录流程

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+  // 小程序关闭 对象进行回收
+  globalData:{
+    token:''
   },
-  globalData: {
-    userInfo: null
-  }
+  onLaunch: function () {
+    // 使用 wx.login接口获取code
+    wx.login({
+      success:(res)=>{
+        const code = res.code
+        wx.request({
+          url: 'http://123.207.32.32:3000/login',
+          methods:'post',
+          data:{
+            code
+          },
+          success:(res)=>{
+            // 1.取出token
+            const token = res.data.token
+            //将token保存到全局变量中
+            this.globalData.token = token
+            // 进行本地存储
+            wx.getStorageInfoSync('token',token)
+          }
+        })   
+      }
+    })
+}
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,52 +3,33 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  showToast(){
+    wx.showToast({
+      title: '加载中',
+      icon:'loading',
+      image:'',
+      mask:true
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  showModal(){
+    wx.showModal({
+      showCancel:true,
+      title:'我是标题',
+      content:'我是内容',
+      cancelText:"退出"
     })
+  },
+  showAction(){
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+    })
+  },
+  onShareAppMessage(){
+    // 返回一个对象 设置分享信息
+   return {
+    title:'你好,李森峰',
+    path:'/pages/index/index',
+    imageUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1601379734150&di=40822daf61ff3b35888146848b9236bd&imgtype=0&src=http%3A%2F%2Ft8.baidu.com%2Fit%2Fu%3D2247852322%2C986532796%26fm%3D79%26app%3D86%26f%3DJPEG%3Fw%3D1280%26h%3D853'
+   } 
   }
 })
